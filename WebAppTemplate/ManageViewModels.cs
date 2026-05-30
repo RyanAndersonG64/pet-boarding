a35341a3c3834f0c49077e8fa88fa0a558b89e92
RyanAndersonG64 <ryan.anderson.g64@gmail.com>
2026-05-30 14:17:22 -0500

    AB#4368 - Removed unnecessary manage views and associated methods

--- WebAppTemplate/ManageViewModels.cs
+++ WebAppTemplate/ManageViewModels.cs
@@ -58,25 +58,7 @@ namespace WebAppTemplate.ViewModels
         public string ConfirmPassword { get; set; }
     }
 
-    public class AddPhoneNumberViewModel
-    {
-        [Required]
-        [Phone]
-        [Display(Name = "Phone Number")]
-        public string Number { get; set; }
-    }
 
-    public class VerifyPhoneNumberViewModel
-    {
-        [Required]
-        [Display(Name = "Code")]
-        public string Code { get; set; }
-
-        [Required]
-        [Phone]
-        [Display(Name = "Phone Number")]
-        public string PhoneNumber { get; set; }
-    }
 
     public class ConfigureTwoFactorViewModel
     {
