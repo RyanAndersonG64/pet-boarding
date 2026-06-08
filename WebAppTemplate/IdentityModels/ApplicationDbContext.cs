d3ef77288720878b213dd43a1793b12e8954c77c
RyanAndersonG64 <ryan.anderson.g64@gmail.com>
2026-06-08 16:21:24 -0500

    AB #4353 - Updated Account View Models

--- WebAppTemplate/IdentityModels/ApplicationDbContext.cs
+++ WebAppTemplate/IdentityModels/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNet.Identity.EntityFramework;
 using WebAppTemplate.IdentityModels;
 
-namespace WebAppTemplate.ViewModels
+namespace WebAppTemplate.IdentityModels
 {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
 
