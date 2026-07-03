85a065c7482694fcef468f1bfaed264963c269f3
RyanAndersonG64 <ryan.anderson.g64@gmail.com>
2026-07-03 12:32:50 -0500

    AB#4362 - Find and remove all remaining instance of WebAppTemplate, delete WebAppTemplate.sln

--- PetBoarding/Migrations/Configuration.cs
+++ PetBoarding/Migrations/Configuration.cs
@@ -1,4 +1,4 @@
-﻿namespace WebAppTemplate.Migrations
+namespace PetBoarding.Migrations
 {
     using System;
     using System.Data.Entity;
--- PetBoarding/Properties/AssemblyInfo.cs
+++ PetBoarding/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebAppTemplate")]
+[assembly: AssemblyTitle("PetBoarding")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebAppTemplate")]
+[assembly: AssemblyProduct("PetBoarding")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
