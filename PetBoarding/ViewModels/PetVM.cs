9b2388395c98a433edde797f5e9f87ccc9c437e8
RyanAndersonG64 <ryan.anderson.g64@gmail.com>
2026-07-03 12:43:01 -0500

    Created PetVM.cs

---
+++ PetBoarding/ViewModels/PetVM.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PetBoarding.Models;
+
+namespace PetBoarding.ViewModels
+{
+    public class ContactFormVM
+    {
+        public ContactFormModel ContactForm { get; set; }
+    }
+}
